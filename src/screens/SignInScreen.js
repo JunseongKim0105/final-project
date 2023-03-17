fb774c8436fb9b74f3a4ddc4a0643589ef2c33ff
JunseongKim0105 <juns.kim0105@gmail.com>
2023-03-17 16:21:10 -0400

    setting project

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,25 @@
+import { Button, StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+const SignInScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>SignInScreen</Text>
+      <Button title="signup" onPress={()=> navigation.navigate('SignUp')}/>
+    </View>
+  );
+};
+
+SignInScreen.propTypes = {};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+  },
+});
+
+export default SignInScreen;
