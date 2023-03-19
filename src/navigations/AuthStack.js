5ce224a0d06ccaabb54c8b8a34cda88d649bf48a
JunseongKim0105 <juns.kim0105@gmail.com>
2023-03-18 22:58:55 -0400

    login view

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { WHITE } from '../colors';
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import { AuthRoutes } from './routes';
@@ -6,7 +7,12 @@ import { AuthRoutes } from './routes';
 const Stack = createNativeStackNavigator();
 const AuthStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        contentStyle: { backgroundColor: WHITE },
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name={AuthRoutes.SIGN_IN} component={SignInScreen} />
       <Stack.Screen name={AuthRoutes.SIGN_UP} component={SignUpScreen} />
     </Stack.Navigator>
