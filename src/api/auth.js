2b7e0a2d7342a06e663fbf12d5f0032521038db6
JunseongKim0105 <juns.kim0105@gmail.com>
2023-03-20 23:40:39 -0400

    add login error message (Jack)

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,21 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  AuthErrorCodes,
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+export const getAuthErrorMessages = (errorCode) => {
+  switch (errorCode) {
+    case AuthErrorCodes.USER_DELETED:
+      return 'cannot found user.';
+    case AuthErrorCodes.INVALID_EMAIL:
+      return 'Invalid Email.';
+    case AuthErrorCodes.INVALID_PASSWORD:
+      return 'Invalid Password.';
+    default:
+      return 'Failed';
+  }
+};
 
 export const signIn = async ({ email, password }) => {
   const { user } = await signInWithEmailAndPassword(getAuth(), email, password);
