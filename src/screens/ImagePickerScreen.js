9c7f0469681b07dc7e8e7c5aa527c1b1c880300d
JunseongKim0105 <juns.kim0105@gmail.com>
2023-03-22 22:24:33 -0400

    got pictures from album

--- src/screens/ImagePickerScreen.js
+++ src/screens/ImagePickerScreen.js
@@ -1,10 +1,60 @@
 import { useNavigation } from '@react-navigation/native';
-import { useLayoutEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import {
+  Alert,
+  FlatList,
+  Image,
+  Pressable,
+  StyleSheet,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import HeaderRight from '../components/HeaderRight';
+import * as MediaLibrary from 'expo-media-library';
 
 const ImagePickerScreen = () => {
   const navigation = useNavigation();
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  const width = useWindowDimensions().width / 3;
+  const [photos, setPhotos] = useState([]);
+  const [listInfo, setListInfo] = useState({
+    endCursor: '',
+    hasNextPage: true,
+  });
+
+  const getPhotos = useCallback(async () => {
+    const options = {
+      first: 30,
+      sortBy: [MediaLibrary.SortBy.creationTime],
+    };
+    if (listInfo.hasNextPage) {
+      const { assets, endCursor, hasNextPage } =
+        await MediaLibrary.getAssetsAsync(options);
+      setPhotos(assets);
+      setListInfo({ endCursor, hasNextPage });
+    }
+  }, [listInfo.hasNextPage]);
+
+  useEffect(() => {
+    if (status?.granted) {
+      getPhotos();
+    }
+  }, [status?.granted, getPhotos]);
+
+  useEffect(() => {
+    (async () => {
+      const { granted } = await requestPermission();
+      if (!granted) {
+        Alert.alert('사진 접근 권한', '사진 접근 권한이 필요합니다.', [
+          {
+            text: 'Confirm',
+            onPress: () => navigation.canGoBack() && navigation.goBack(),
+          },
+        ]);
+      }
+    })();
+  }, [navigation,requestPermission]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -13,9 +63,16 @@ const ImagePickerScreen = () => {
   });
   return (
     <View style={styles.container}>
-      <Text style={styles.title} ImagePickerScreen>
-        ImagePickerScreen
-      </Text>
+      <FlatList
+        style={styles.list}
+        data={photos}
+        renderItem={({ item }) => (
+          <Pressable style={{ width, height: width }}>
+            <Image source={{ uri: item.uri }} style={styles.photo} />
+          </Pressable>
+        )}
+        numColumns={3}
+      />
     </View>
   );
 };
@@ -26,8 +83,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  title: {
-    fontSize: 30,
+  list: {
+    width: '100%',
+  },
+  photo: {
+    width: '100%',
+    height: '100%',
   },
 });
 
