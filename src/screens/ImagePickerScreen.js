559602157a76edda10d7a9d6341d0b129e094088
JunseongKim0105 <juns.kim0105@gmail.com>
2023-03-23 00:54:14 -0400

    add image selected function

--- src/screens/ImagePickerScreen.js
+++ src/screens/ImagePickerScreen.js
@@ -10,6 +10,7 @@ import {
   Alert,
   FlatList,
   Image,
+  Platform,
   Pressable,
   StyleSheet,
   useWindowDimensions,
@@ -17,6 +18,14 @@ import {
 } from 'react-native';
 import HeaderRight from '../components/HeaderRight';
 import * as MediaLibrary from 'expo-media-library';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { PRIMARY } from '../colors';
+import { BlurView } from 'expo-blur';
+
+const initListInfo = {
+  endCursor: '',
+  hasNextPage: true,
+};
 
 const ImagePickerScreen = () => {
   const navigation = useNavigation();
@@ -38,10 +47,9 @@ const ImagePickerScreen = () => {
 
   const width = useWindowDimensions().width / 3;
   const [photos, setPhotos] = useState([]);
-  const listInfo = useRef({
-    endCursor: '',
-    hasNextPage: true,
-  });
+  const listInfo = useRef(initListInfo);
+  const [refreshing, setRefreshing] = useState(false);
+  const [selectedPhotos, setSelectedPhotos] = useState([]);
 
   const getPhotos = useCallback(async () => {
     const options = {
@@ -56,12 +64,17 @@ const ImagePickerScreen = () => {
     if (listInfo.current.hasNextPage) {
       const { assets, endCursor, hasNextPage } =
         await MediaLibrary.getAssetsAsync(options);
-      setPhotos((prev) => [...prev, ...assets]);
-      listInfo.current({ endCursor, hasNextPage });
+      setPhotos((prev) => (options.after ? [...prev, ...assets] : assets));
+      listInfo.current = { endCursor, hasNextPage };
     }
   }, []);
 
-  console.log(photos.length);
+  const onRefresh = async () => {
+    setRefreshing(true);
+    listInfo.current = initListInfo;
+    await getPhotos();
+    setRefreshing(false);
+  };
 
   useEffect(() => {
     if (status?.granted) {
@@ -74,19 +87,56 @@ const ImagePickerScreen = () => {
       headerRight: () => <HeaderRight onPress={() => {}} />,
     });
   });
+
+  const isSelectedPhoto = (photo) => {
+    return selectedPhotos.findIndex((item) => item.id === photo.id) > -1;
+  };
+
+  const togglePhoto = (photo) => {
+    const isSelected = isSelectedPhoto(photo);
+    setSelectedPhotos((prev) =>
+      isSelected
+        ? prev.filter((item) => item.id !== photo.id)
+        : [...prev, photo]
+    );
+  };
+
+  console.log(selectedPhotos);
+
   return (
     <View style={styles.container}>
       <FlatList
         style={styles.list}
         data={photos}
-        renderItem={({ item }) => (
-          <Pressable style={{ width, height: width }}>
-            <Image source={{ uri: item.uri }} style={styles.photo} />
-          </Pressable>
-        )}
+        renderItem={({ item }) => {
+          const isSelected = isSelectedPhoto(item);
+
+          return (
+            <Pressable
+              onPress={() => togglePhoto(item)}
+              style={{ width, height: width }}
+            >
+              <Image source={{ uri: item.uri }} style={styles.photo} />
+              {isSelected && (
+                <BlurView
+                  intensity={Platform.select({ ios: 10, android: 80 })}
+                  style={[StyleSheet.absoluteFillObject, styles.checkIcon]}
+                >
+                  <MaterialCommunityIcons
+                    name="check-circle"
+                    size={40}
+                    color={PRIMARY.DEFAULT}
+                  />
+                </BlurView>
+              )}
+            </Pressable>
+          );
+        }}
         numColumns={3}
         onEndReached={getPhotos}
         onEndReachedThreshold={0.3}
+        onRefresh={onRefresh}
+        refreshing={refreshing}
       />
     </View>
   );
@@ -104,6 +154,10 @@ const styles = StyleSheet.create({
   photo: {
     width: '100%',
     height: '100%',
+  },
+  checkIcon: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
