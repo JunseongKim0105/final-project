a09330ba10fdf28b69de6d44630e3930758e97e5
JunseongKim0105 <juns.kim0105@gmail.com>
2023-03-24 15:23:42 -0400

    can refreshing and get next page on listscreen

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,27 +3,33 @@ import PropTypes from 'prop-types';
 import PostItem from './PostItem';
 import { GRAY } from '../colors';
 
-const PostList = ({ data }) => {
+const PostList = ({ data, fetchNextPage,refreshing,refetch }) => {
   return (
     <FlatList
-    data={data}
-    renderItem={({item})=><PostItem post={item}/>}
-    ItemSeparatorComponent={()=><View style={styles.seperator}></View>}
+      data={data}
+      renderItem={({ item }) => <PostItem post={item} />}
+      ItemSeparatorComponent={() => <View style={styles.seperator}></View>}
+      onEndReached={fetchNextPage}
+      onEndReachedThreshold={0.4}
+      onRefresh={refetch}
+      refreshing={refreshing}
     />
   );
 };
 
 PostList.propTypes = {
   data: PropTypes.array.isRequired,
+  fetchNextPage: PropTypes.func,
+  refetch: PropTypes.func,
+  refreshing:PropTypes.bool
 };
 
 const styles = StyleSheet.create({
   seperator: {
-   marginVertical:20,
-   borderBottomWidth:0.5,
-   borderBottomColor:GRAY.LIGHT,
+    marginVertical: 20,
+    borderBottomWidth: 0.5,
+    borderBottomColor: GRAY.LIGHT,
   },
-  
 });
 
 export default PostList;
